65c6a82d96f2c0a6ce639306c411ea538a5b32f1
noprysk <oprysk.nazar@gmail.com>
2018-09-10 19:54:17 -0600

    Set Repository annotation.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-microservice-forex-service
+
+http://localhost:8000/currency-exchange/from/EUR/to/INR
--- src/main/java/nao/cycledev/forex/service/repositories/ExchangeValueRepository.java
+++ src/main/java/nao/cycledev/forex/service/repositories/ExchangeValueRepository.java
@@ -2,7 +2,9 @@ package nao.cycledev.forex.service.repositories;
 
 import nao.cycledev.forex.service.modal.ExchangeValue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
 
     ExchangeValue findByFromAndTo(String from, String to);
